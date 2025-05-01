6eef4c378d4462495a9517ba161a907e318ea9dd
Mohammed1768 <mohammedwissam728@gmail.com>
2025-05-01 16:32:48 +0300

    Update Project.py

--- Project.py
+++ Project.py
@@ -8,14 +8,19 @@ import sounddevice as sd
 frequencies_3rd = np.array([220.00, 246.94, 130.81, 146.83, 164.81, 174.61, 196.00, 220.00])
 frequencies_4th = 2 * frequencies_3rd
 
-N = 5 # Number of sound samples
+N = 50 # Number of sound samples
+time = 10
 
-t = np.linspace(0, 3, 3 * 44100) # 3 seconds of time
+t = np.linspace(0, 10, 3 * 44100) # 3 seconds of time
 F = np.random.choice(frequencies_3rd, N) # Random frequencies from the 4th octave
 f = np.random.choice(frequencies_4th, N) # Random frequencies from the 3rd octave
-T = np.random.choice(1, 4, N) # Random time from 1 to 4 seconds
+T = np.random.uniform(1, 4, N) # Random time from 1 to 4 seconds
 
-harmonics = np.sin(2*np.pi*f*t) + np.sin(2*np.pi*F*t) * (t<=T) # Harmonics of the frequencies
+harmonics = np.array([np.sin(2 * np.pi * f[i] * t) + 
+                    np.sin(2 * np.pi * F[i] * t) * (t <= T[i]) for i in range(N)])
+# Create the harmonics
+# Each harmonic is a sum of two sine waves: one from the 3rd octave and one from the 4th octave
+
 x = np.sum(harmonics, axis=0) # Sum of the harmonics
 
 plt.plot(t, x) # Plot the sound
